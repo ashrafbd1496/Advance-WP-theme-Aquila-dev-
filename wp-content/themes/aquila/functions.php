95369bff80abf95cea435952a29a180123f8e666
ashrafbd1496 <asraf1496@gmail.com>
2022-05-16 21:42:41 +0600

    added bootstrap compiled css and js file

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -14,9 +14,17 @@ add_action( 'wp_enqueue_scripts', 'aquila_scripts' );
 function aquila_scripts(){
     //Register styles and scripts
     wp_register_style('main-style',get_stylesheet_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
+    wp_register_style('bootstrap-css',get_template_directory_uri() . '/assets/src/library/bootstrap.min.css', array(),false, 'all');
+
+
+
     wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js',[],filemtime(get_template_directory() . '/assets/main.js'), false);
+     wp_register_script('bootstrap-js',get_template_directory_uri() . '/assets/src/library/bootstrap.min.js', array('jquery'),false, true);
 
     //Enqueue Styles and Scripts
     wp_enqueue_style('main-style');
+    wp_enqueue_style('bootstrap-css');
+
     wp_enqueue_script('main-js');
+    wp_enqueue_script('bootstrap-js');
 }
--- wp-content/themes/aquila/header.php
+++ wp-content/themes/aquila/header.php
@@ -13,5 +13,7 @@
    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
+    <?php if (function_exists('wp_body_open')) {
+     wp_body_open();
+    }?>
     <div class="header">Header</div>
