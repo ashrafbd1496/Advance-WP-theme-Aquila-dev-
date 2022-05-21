c38c1dc655e8b25bfcb38bd011070484a7625c38
ashrafbd1496 <asraf1496@gmail.com>
2022-05-21 20:08:20 +0600

    has made bootstrap menu  and child menu dynamic

--- wp-content/themes/aquila/inc/classes/class-menus.php
+++ wp-content/themes/aquila/inc/classes/class-menus.php
@@ -38,7 +38,16 @@ class Menus{
 		//return ! empty($menu_id) ? $menu_id : '';
 
 	}
-
-
-
+	public function get_child_menu_items($menu_array, $parent_id){
+		$child_menus = [];
+		if(!empty($menu_array) && is_array($menu_array)){
+			foreach($menu_array as $menu){
+				if (intval($menu->menu_item_parent) === $parent_id) {
+					array_push($child_menus, $menu);
+				}
+			}
+		}
+		return $child_menus;
+	}
+	
 } //menu class end
