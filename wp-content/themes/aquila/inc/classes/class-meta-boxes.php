083a636a58694dbac0034820bedec0290aa80688
ashrafbd1496 <asraf1496@gmail.com>
2022-06-09 11:26:14 +0600

    displayed post titles

--- wp-content/themes/aquila/inc/classes/class-meta-boxes.php
+++ wp-content/themes/aquila/inc/classes/class-meta-boxes.php
@@ -34,8 +34,13 @@ class Meta_Boxes{
 	}
 	public function custom_meta_box_html($post){
 		 $value = get_post_meta( $post->ID, '_hide_page_title', true ); 
-		 ?>
 
+		 /**
+		  * use nonce for verification
+		  */
+		 wp_nonce_field( plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name' );
+		 ?>
+	
 	    <label for="aquila-filed"><?php esc_html_e('Hide the page title','aquila'); ?></label>
 	    <select name="aquila_hide_title_filed" id="aquila-filed" class="postbox">
 	        <option value=""><?php esc_html_e('Select','aquila'); ?></option>
@@ -50,6 +55,22 @@ class Meta_Boxes{
 	<?php }
 
 	public function save_post_meta_data($post_id){
+		/**
+		 * when post saved or updated we get _POST available 
+		 * Check if the current user is authorized 
+		 */
+		if ( ! current_user_can('edit_post', $post_id)) {
+			return ;
+		}
+		/**
+		 * check if the nonce value we received is the same we created. 
+		 */
+		if ( ! isset( $_POST['hide_title_meta_box_nonce_name'] ) 
+			    || ! wp_verify_nonce( $_POST['hide_title_meta_box_nonce_name'], plugin_basename(__FILE__)) 
+			) {
+			 return ;
+			}
+		
 		 if ( array_key_exists( 'aquila_hide_title_filed', $_POST ) ) {
 	        update_post_meta(
 	            $post_id,
--- wp-content/themes/aquila/template-parts/components/blog/entry-header.php
+++ wp-content/themes/aquila/template-parts/components/blog/entry-header.php
@@ -5,6 +5,15 @@
  */
 
 $the_post_id = get_the_ID();
+$hide_title = get_post_meta($the_post_id,'_hide_page_title', true);
+$heading_class = !empty($hide_title) && 'yes' === $hide_title ? 'hide' : '';
+
+/**
+    echo '<pre>';
+	print_r($hide_title);
+	wp_die();
+ */
+
 $has_post_thumbnail = get_the_post_thumbnail($the_post_id);
 
  ?>
@@ -29,6 +38,22 @@ $has_post_thumbnail = get_the_post_thumbnail($the_post_id);
 			</a>
 			</div>
 		<?php }
+
+		//Title
+		if (is_single() || is_page()) {
+			printf(
+				'<h1 class="page-title text-dark %1$s">%2$s</h1>',
+				esc_attr($heading_class),
+				wp_kses_post( get_the_title() )
+			);
+		}else{
+			printf(
+				'<h2 class="entry-title mb-3"><a class="text-dark" hfref="%1$s">%2$s</a></h2>',
+				esc_url(get_the_permalink( )),
+				wp_kses_post( get_the_title() )
+			);
+		}
+		
 	 ?>
 	
 </header>
